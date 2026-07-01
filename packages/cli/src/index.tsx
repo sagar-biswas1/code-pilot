eaef6aacb2399d14ec4e2687a2ee4367d96225b0
sagar-biswas1 <bsagor99@gmail.com>
2026-07-02 00:04:51 +0600

    Update package.json to include development script and modify CLI component to replace text display with a textarea for user input.

--- packages/cli/src/index.tsx
+++ packages/cli/src/index.tsx
@@ -5,8 +5,7 @@ function App() {
   return (
     <box alignItems="center" justifyContent="center" flexGrow={1}>
       <box justifyContent="center" alignItems="flex-end">
-        <ascii-font font="tiny" text="OpenTUI" />
-        <text attributes={TextAttributes.DIM}>What will you build?</text>
+        <textarea placeholder="Enter your code here" />
       </box>
     </box>
   );
